fcbb8fe8bb1a674ac91d0194961691596243f4cf
Brylle Justin Heraldo <bryllejheraldo@gmail.com>
2023-10-07 23:27:59 +0800

    Fix PlacePACD, PlacePortal, PlaceCashier, and PlaceBiometric mathematical path

--- LTO_QueueingSimulation.py
+++ LTO_QueueingSimulation.py
@@ -5,22 +5,22 @@ import time
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Percentage = (Number / Total) x 100
 
-# Student Permit – 30 to 50  = 20% Yellow Card
-# Renewal + Examination – 80 to 100; = 40% Blue Card
-# NonPro + Examination – 50 to 70  = 28% Green Card
-# Miscellaneous – 20 to 30 = 12% Orange Card
+# Student Permit – 30 to 50  = 34% Yellow Card
+# Renewal + Examination – 30 to 40; = 28% Blue Card
+# NonPro + Examination – 25 to 35  = 24% Green Card
+# Miscellaneous – 10 to 20 = 14% Orange Card
 
-# total = 250
+# total = 145
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# The average LTO applicants per day is around 200 - 250
-applicants = random.randint(200, 250)
+# The average LTO applicants per day is around 95 - 145
+applicants = random.randint(95, 145)
 
 # Total Average of Applicant in seperate Licensing queueing
-yellow_card = applicants * 0.2
-blue_card = applicants * 0.4
-green_card = applicants * 0.28
-orange_card = applicants * 0.12
+yellow_card = applicants * 0.34
+blue_card = applicants * 0.28
+green_card = applicants * 0.24
+orange_card = applicants * 0.14
 
 
 print("Total Student Permit Applicants: ", int(yellow_card))
@@ -32,34 +32,52 @@ print("")
 
 
 def PlacePACD():
-    # Consume atleast 30 secs per person however there's a queueing
+    # Consume at least 30 seconds per person; however, there's a queuing
     # Formula Total Time in MINUTES = (Number of People) x (Time per Person in seconds) / 60
     # Total Time in HOURS = MINUTES / 60
 
     PACD_total_minutes = applicants * 30 / 60
     PACD_total_hours = PACD_total_minutes / 60
 
-    PACD_TimeConsume = (PACD_total_hours / 2 + float(PACD))  #By adding more PACD it will divide the time depending on how many they set up
-    print("Total PACD Time (in hours):", PACD_TimeConsume)
+    PACD_TimeConsume = (PACD_total_hours / float(PACD))
+
+    # Convert PACD_TimeConsume into hours and minutes
+    hours = int(PACD_TimeConsume)
+    minutes = int((PACD_TimeConsume - hours) * 60)
+
+    print("Total PACD Time: {} hours and {} minutes".format(hours, minutes))
+
+
 
 
 
 def PlacePortal():
+
     # Consume atleast 1 minute per person = 60 secs
     portal_total_minutes = applicants * 60 / 60
     portal_total_hours = portal_total_minutes / 60
 
-    portal_TimeConsume = (portal_total_hours / float(Portal))
-    print("Total Portal Time (in hours):", portal_TimeConsume)
+    Portal_TimeConsume = (portal_total_hours / float(Portal))
+
+    # Convert PACD_TimeConsume into hours and minutes
+    hours = int(Portal_TimeConsume)
+    minutes = int((Portal_TimeConsume - hours) * 60)
+
+    print("Total Portal Time: {} hours and {} minutes".format(hours, minutes))
 
 
 def PlaceCashier():
-    #consume atleast 1 minute per person = 60 secs
-    Cashier_total_minutes = applicants * 60 / 60
+    #consume atleast 1 and 10 secs minute per person = 70 secs
+    Cashier_total_minutes = applicants * 70 / 60
     Cashier_total_hours = Cashier_total_minutes / 60
 
     Cashier_TimeConsume = (Cashier_total_hours / float(Cashier))
-    print("Total Accounting Time (in hours):",Cashier_TimeConsume)
+
+    # Convert PACD_TimeConsume into hours and minutes
+    hours = int(Cashier_TimeConsume)
+    minutes = int((Cashier_TimeConsume - hours) * 60)
+
+    print("Total Cashier Time: {} hours and {} minutes".format(hours, minutes))
 
 
 def PlaceComputer():
@@ -87,18 +105,32 @@ def PlaceComputer():
 
     # Convert the total times from seconds to minutes
     total_both_card_time_minutes = (total_blue_card_time + total_green_card_time) / 60
-    total_both_card_time_hours = total_both_card_time_minutes /  60
+    total_both_card_time_hours = total_both_card_time_minutes / 60
 
     ExaminationTimeConsume = total_both_card_time_hours / 7 + float(Computer)
 
     print("Total Blue & Green Card Time (in hours):", ExaminationTimeConsume)
 
+    # To be fixed
 
 
 
+def PlaceBiometric():
+    # Consume at least 40 seconds per person; however, there's a queuing
+    # Formula Total Time in MINUTES = (Number of People) x (Time per Person in seconds) / 60
+    # Total Time in HOURS = MINUTES / 60
+
+    Biometric_total_minutes = applicants * 40 / 60
+    Biometric_total_hours = Biometric_total_minutes / 60
+
+    Biometric_TimeConsume = (Biometric_total_hours / float(Biometric))
 
 
+    # Convert PACD_TimeConsume into hours and minutes
+    hours = int(Biometric_TimeConsume)
+    minutes = int((Biometric_TimeConsume - hours) * 60)
 
+    print("Total Biometric Time: {} hours and {} minutes".format(hours, minutes))
 
 
 # Item to be added
@@ -115,6 +147,6 @@ PlacePACD()
 PlacePortal()
 PlaceCashier()
 PlaceComputer()
-
+PlaceBiometric()
 
 
