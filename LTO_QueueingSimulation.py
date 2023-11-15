bbd8fe209976f1f4281455963791a9312721ae99
Brylle Justin Heraldo <bryllejheraldo@gmail.com>
2023-11-15 13:47:08 +0800

    Putting the console base inside the GUI (40%)

--- LTO_QueueingSimulation.py
+++ LTO_QueueingSimulation.py
@@ -6,6 +6,306 @@ from tkinter import Tk, Label, Button, Entry, LabelFrame, font, PhotoImage
 from PIL import ImageTk, Image
 import os
 
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  Guide
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Percentage = (Number / Total) x 100
+
+# Student Permit – 30 to 50  = 34% Yellow Card
+# Renewal + Examination – 30 to 40; = 28% Blue Card
+# NonPro + Examination – 25 to 35  = 24% Green Card
+# Miscellaneous – 10 to 20 = 14% Orange Card
+
+# total = 145
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# The average LTO applicants per day is around 95 - 145
+applicants = random.randint(95, 145)
+
+# Total Average of Applicant in seperate Licensing queueing
+yellow_card = applicants * 0.34
+blue_card = applicants * 0.28
+green_card = applicants * 0.24
+orange_card = applicants * 0.14
+
+
+def PlacePACD():
+    total_time_minutes = 0
+    total_time_hours = 0
+    individual_times = []
+    PACD_result = PACD_entry.get()
+
+    for _ in range(applicants):
+        PACD_time_avg = random.randint(30, 60)
+        PACD_total_minutes = PACD_time_avg / 60
+        total_time_minutes += PACD_total_minutes
+        individual_times.append(PACD_time_avg)
+
+    PACD_total_time_hours = total_time_minutes / 60
+
+    try:
+        PACD_TimeConsume = PACD_total_time_hours / (2 + float(PACD_result))
+
+        # Convert PACD_TimeConsume into hours and minutes
+        hours = int(PACD_TimeConsume)
+        minutes = int((PACD_TimeConsume - hours) * 60)
+
+        print("Total PACD Time: {} hours and {} minutes".format(hours, minutes))
+
+        # Print individual times
+        for i, time_avg in enumerate(individual_times, 1):
+            individual_seconds = time_avg
+            individual_minutes = individual_seconds // 60
+            individual_seconds = individual_seconds % 60
+
+            #print each individual timeframe
+            # print("Applicant {}: {} seconds ({} minutes and {} seconds)".format(i, time_avg, individual_minutes, individual_seconds))
+
+    except ValueError:
+        PACD_TimeConsume = PACD_total_time_hours / 2
+
+        # Convert PACD_TimeConsume into hours and minutes
+        hours = int(PACD_TimeConsume)
+        minutes = int((PACD_TimeConsume - hours) * 60)
+
+        print("Total PACD Time: {} hours and {} minutes".format(hours, minutes))
+
+    except ZeroDivisionError:
+        PACD_TimeConsume = PACD_total_time_hours / 2
+
+        # Convert PACD_TimeConsume into hours and minutes
+        hours = int(PACD_TimeConsume)
+        minutes = int((PACD_TimeConsume - hours) * 60)
+
+        print("Total PACD Time: {} hours and {} minutes".format(hours, minutes))
+
+def PlacePortal():
+    Portal_total_time_minutes = 0
+    Portal_total_time_hours = 0
+    Portal_individual_times = []
+    Portal_result = Portal_entry.get()
+
+    for _ in range(applicants):
+        Portal_time_avg = random.randint(60, 90)
+        Portal_total_minutes = Portal_time_avg / 60
+        Portal_total_time_minutes += Portal_total_minutes
+        Portal_individual_times.append(Portal_time_avg)
+
+    Portal_total_time_hours = Portal_total_time_minutes / 60
+
+    try:
+        Portal_TimeConsume = (Portal_total_time_hours / float(Portal_result))
+
+        # Convert Portal_TimeConsume into hours and minutes
+        hours = int(Portal_TimeConsume)
+        minutes = int((Portal_TimeConsume - hours) * 60)
+
+        print("Total Portal Time: {} hours and {} minutes".format(hours, minutes))
+
+        # Print individual times
+        for Portal_i, Portal_time_avg in enumerate(Portal_individual_times, 1):
+            Portal_individual_seconds = Portal_time_avg
+            Portal_individual_minutes = Portal_individual_seconds // 60
+            Portal_individual_seconds = Portal_individual_seconds % 60
+
+
+            #Print each individual TimeFrame
+
+
+            # print("Portal Applicant {}: {} seconds ({} minutes and {} seconds)".format(Portal_i, Portal_time_avg, Portal_individual_minutes,  Portal_individual_seconds))
+
+
+
+    except ValueError:
+        Portal_TimeConsume = (Portal_total_time_hours / 1)
+
+        # Convert Portal_TimeConsume into hours and minutes
+        hours = int(Portal_TimeConsume)
+        minutes = int((Portal_TimeConsume - hours) * 60)
+
+        print("Total Portal Time: {} hours and {} minutes".format(hours, minutes))
+
+    except ZeroDivisionError:
+        Portal_TimeConsume = (Portal_total_time_hours / 1)
+
+        # Convert Portal_TimeConsume into hours and minutes
+        hours = int(Portal_TimeConsume)
+        minutes = int((Portal_TimeConsume - hours) * 60)
+
+        print("Total Portal Time: {} hours and {} minutes".format(hours, minutes))
+
+def PlaceCashier():
+    Cashier_total_time_minutes = 0
+    Cashier_total_time_hours = 0
+    Cashier_individual_times = []
+    Cashier_result = Cashier_entry.get()
+
+    for _ in range(applicants):
+        Cashier_time_avg = random.randint(40, 80)  #consume atleast 40secs to 1min and 20secs individual
+        Cashier_total_minutes = Cashier_time_avg / 60
+        Cashier_total_time_minutes += Cashier_total_minutes
+        Cashier_individual_times.append(Cashier_time_avg)
+
+    Cashier_total_time_hours = Cashier_total_time_minutes / 60
+
+
+    try:
+        Cashier_TimeConsume = (Cashier_total_time_hours/ float(Cashier_result))
+
+        # Convert Cashier_TimeConsume into hours and minutes
+        hours = int(Cashier_TimeConsume)
+        minutes = int((Cashier_TimeConsume - hours) * 60)
+
+        print("Total Cashier Time: {} hours and {} minutes".format(hours, minutes))
+
+        # Print individual times
+        for Cashier_i, Cashier_time_avg in enumerate(Cashier_individual_times, 1):
+            Cashier_individual_seconds = Cashier_time_avg
+            Cashier_individual_minutes = Cashier_individual_seconds // 60
+            Cashier_individual_seconds = Cashier_individual_seconds % 60
+            # print("Applicant {}: {} seconds ({} minutes and {} seconds)".format(Cashier_i, Cashier_time_avg, Cashier_individual_minutes, Cashier_individual_seconds))
+
+
+    except ValueError:
+        Cashier_TimeConsume = (Cashier_total_time_hours / 1)
+
+        # Convert Cashier_TimeConsume into hours and minutes
+        hours = int(Cashier_TimeConsume)
+        minutes = int((Cashier_TimeConsume - hours) * 60)
+
+        print("Total Cashier Time: {} hours and {} minutes".format(hours, minutes))
+
+    except ZeroDivisionError:
+        Cashier_TimeConsume = (Cashier_total_time_hours / 1)
+
+        # Convert Cashier_TimeConsume into hours and minutes
+        hours = int(Cashier_TimeConsume)
+        minutes = int((Cashier_TimeConsume - hours) * 60)
+
+        print("Total Cashier Time: {} hours and {} minutes".format(hours, minutes))
+
+def PlaceComputer():
+    Computer_result = Computer_entry.get()
+    # Define the range for time allocation (10 minutes to 1 hour in seconds)
+    min_time = 10 * 60
+    max_time = 60 * 60
+
+    # Define the number of blue card and green card applicants
+    blue_card_count = int(blue_card)
+    green_card_count = int(green_card)
+
+    # Initialize variables to store the total times for blue card and green card applicants
+    total_blue_card_time = 0
+    total_green_card_time = 0
+
+    # Generate random times for blue card applicants and calculate the total time
+    for _ in range(blue_card_count):
+        time_allocated = random.randint(min_time, max_time)
+        total_blue_card_time += time_allocated / 7   # to be fixed
+
+    # Generate random times for green card applicants and calculate the total time
+    for _ in range(green_card_count):
+        time_allocated = random.randint(min_time, max_time)
+        total_green_card_time += time_allocated / 7    # to be fixed
+
+    # Convert the total times from seconds to minutes
+    total_both_card_time_minutes = (total_blue_card_time + total_green_card_time) / 60
+    total_both_card_time_hours = total_both_card_time_minutes / 60
+
+
+    try:
+        ExaminationTimeConsume = total_both_card_time_hours / (7 + float(Computer_result))
+
+        # Convert Examination_TimeConsume into hours and minutes
+        hours = int(ExaminationTimeConsume)
+        minutes = int((ExaminationTimeConsume - hours) * 60)
+
+        print("Total Examation Time: {} hours and {} minutes".format(hours, minutes))
+
+
+
+    except ValueError:
+        ExaminationTimeConsume = total_both_card_time_hours / 7
+
+        # Convert Examination_TimeConsume into hours and minutes
+        hours = int(ExaminationTimeConsume)
+        minutes = int((ExaminationTimeConsume - hours) * 60)
+
+        print("Total Examation Time: {} hours and {} minutes".format(hours, minutes))
+
+    except ZeroDivisionError:
+        ExaminationTimeConsume = total_both_card_time_hours / 7
+
+        # Convert Examination_TimeConsume into hours and minutes
+        hours = int(ExaminationTimeConsume)
+        minutes = int((ExaminationTimeConsume - hours) * 60)
+
+        print("Total Examation Time: {} hours and {} minutes".format(hours, minutes))
+
+
+# To be fixed Dapat sa random ppl first 7 ang makaka accomodate ng room
+
+
+def PlaceBiometric():
+    # Consume at least 40 seconds per person; however, there's a queuing
+    # Formula Total Time in MINUTES = (Number of People) x (Time per Person in seconds) / 60
+    # Total Time in HOURS = MINUTES / 60
+
+    Biometric_total_time_minutes = 0
+    Biometric_total_time_hours = 0
+    Biometric_individual_times = []
+    Biometric_result = Biometric_entry.get()
+
+    for _ in range(applicants):
+        Biometric_time_avg = random.randint(40, 80)
+        Biometric_total_minutes = Biometric_time_avg / 60
+        Biometric_total_time_minutes += Biometric_total_minutes
+        Biometric_individual_times.append(Biometric_time_avg)
+
+    Biometric_total_time_hours = Biometric_total_time_minutes / 60
+
+
+    try:
+
+        Biometric_TimeConsume = (Biometric_total_time_hours / float(Biometric_result))
+
+
+        # Convert PACD_TimeConsume into hours and minutes
+        hours = int(Biometric_TimeConsume)
+        minutes = int((Biometric_TimeConsume - hours) * 60)
+
+        print("Total Biometric Time: {} hours and {} minutes".format(hours, minutes))
+
+        # Print individual times
+        for Biometric_i, Biometric_time_avg in enumerate(Biometric_individual_times, 1):
+            Biometric_individual_seconds = Biometric_time_avg
+            Biometric_individual_minutes = Biometric_individual_seconds // 60
+            Biometric_individual_seconds = Biometric_individual_seconds % 60
+
+            # print each individual timeframe
+            # print("Applicant {}: {} seconds ({} minutes and {} seconds)".format(i, time_avg, individual_minutes, individual_seconds))
+
+
+
+    except ValueError:
+        Biometric_TimeConsume = (Biometric_total_time_hours / 1)
+
+        # Convert PACD_TimeConsume into hours and minutes
+        hours = int(Biometric_TimeConsume)
+        minutes = int((Biometric_TimeConsume - hours) * 60)
+
+        print("Total Biometric Time: {} hours and {} minutes".format(hours, minutes))
+
+    except ZeroDivisionError:
+        Biometric_TimeConsume = (Biometric_total_time_hours / 1)
+
+        # Convert PACD_TimeConsume into hours and minutes
+        hours = int(Biometric_TimeConsume)
+        minutes = int((Biometric_TimeConsume - hours) * 60)
+
+        print("Total Biometric Time: {} hours and {} minutes".format(hours, minutes))
+
 def guide_butt():
     # Direct to INSTRUCTION GUI
     print("Button clicked!")
@@ -93,6 +393,58 @@ def Simulation_Mode():
         title_text2.destroy()
         # write result code here
 
+    def PACD_entry_focus_in(event):
+        if PACD_entry.get() == "Enter number":
+            PACD_entry.delete(0, 'end')
+            PACD_entry.config(fg="Black")
+
+    def PACD_entry_focus_out(event):
+        if PACD_entry.get() == "":
+            PACD_entry.insert(0, "Enter number")
+            PACD_entry.config(fg="gray")
+
+    def Portal_entry_focus_in(event):
+        if Portal_entry.get() == "Enter number":
+            Portal_entry.delete(0, 'end')
+            Portal_entry.config(fg="Black")
+
+    def Portal_entry_focus_out(event):
+        if Portal_entry.get() == "":
+            Portal_entry.insert(0, "Enter number")
+            Portal_entry.config(fg="gray")
+
+    def Cashier_entry_focus_in(event):
+        if Cashier_entry.get() == "Enter number":
+            Cashier_entry.delete(0, 'end')
+            Cashier_entry.config(fg="Black")
+
+    def Cashier_entry_focus_out(event):
+        if Cashier_entry.get() == "":
+            Cashier_entry.insert(0, "Enter number")
+            Cashier_entry.config(fg="gray")
+
+    def Computer_entry_focus_in(event):
+        if Computer_entry.get() == "Enter number":
+            Computer_entry.delete(0, 'end')
+            Computer_entry.config(fg="Black")
+
+    def Computer_entry_focus_out(event):
+        if Computer_entry.get() == "":
+            Computer_entry.insert(0, "Enter number")
+            Computer_entry.config(fg="gray")
+
+    def Biometric_entry_focus_in(event):
+        if Biometric_entry.get() == "Enter number":
+            Biometric_entry.delete(0, 'end')
+            Biometric_entry.config(fg="Black")
+
+    def Biometric_entry_focus_out(event):
+        if Biometric_entry.get() == "":
+            Biometric_entry.insert(0, "Enter number")
+            Biometric_entry.config(fg="gray")
+
+
+
     sim = Tk()
     sim.title("LTO Licensing Queueing System Simulation")
     sim.geometry("1374x751")
@@ -128,6 +480,9 @@ def Simulation_Mode():
 
     PACD_entry = Entry(first_frame, font=mont_normal, bd=5)
     PACD_entry.grid(row=1, column=0, ipadx=25, padx=10, pady=10, sticky='ne')
+    PACD_entry.insert(0, 'Enter number')
+    PACD_entry.bind("<FocusIn>", PACD_entry_focus_in)
+    PACD_entry.bind("<FocusOut>", PACD_entry_focus_out)
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Portal Station
@@ -136,6 +491,9 @@ def Simulation_Mode():
 
     Portal_entry = Entry(first_frame, bd=5, font=mont_normal)
     Portal_entry.grid(row=2, column=0, ipadx=25, padx=10, pady=10, sticky='ne')
+    Portal_entry.insert(0, 'Enter number')
+    Portal_entry.bind("<FocusIn>", Portal_entry_focus_in)
+    Portal_entry.bind("<FocusOut>", Portal_entry_focus_out)
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Cashier Station
@@ -144,6 +502,9 @@ def Simulation_Mode():
 
     Cashier_entry = Entry(first_frame, bd=5, font=mont_normal)
     Cashier_entry.grid(row=3, column=0, ipadx=25, padx=10, pady=10, sticky='ne')
+    Cashier_entry.insert(0, 'Enter number')
+    Cashier_entry.bind("<FocusIn>", Cashier_entry_focus_in)
+    Cashier_entry.bind("<FocusOut>", Cashier_entry_focus_out)
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Examination Station
@@ -152,6 +513,9 @@ def Simulation_Mode():
 
     Computer_entry = Entry(first_frame, bd=5, font=mont_normal)
     Computer_entry.grid(row=4, column=0, ipadx=25, padx=10, pady=10, sticky='ne')
+    Computer_entry.insert(0, 'Enter number')
+    Computer_entry.bind("<FocusIn>", Computer_entry_focus_in)
+    Computer_entry.bind("<FocusOut>", Computer_entry_focus_out)
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Biometric Station
@@ -160,8 +524,13 @@ def Simulation_Mode():
 
     Biometric_entry = Entry(first_frame, bd=5, font=mont_normal)
     Biometric_entry.grid(row=5, column=0, ipadx=25, padx=10, pady=10, sticky='ne')
+    Biometric_entry.insert(0, 'Enter number')
+    Biometric_entry.bind("<FocusIn>", Biometric_entry_focus_in)
+    Biometric_entry.bind("<FocusOut>", Biometric_entry_focus_out)
 
-    Simulation_Button = Button(first_frame, text="SIMULATE", fg="#000000", font=mont_bold, width=10, height=2, bd=5,command=showResults)
+    Simulation_Button = Button(first_frame, text="SIMULATE", fg="#000000", font=mont_bold, width=10, height=2, bd=5,
+    command=lambda: (showResults(), PlacePACD(), PlacePortal(), PlaceCashier(), PlaceComputer(), PlaceBiometric()))
+
     Simulation_Button.bind("<Enter>", simulateButtonEnter)
     Simulation_Button.bind("<Leave>", simulateButtonLeave)
     Simulation_Button.grid(row=6, column=0, ipadx=90, padx=10, pady=40, sticky='nsew')
@@ -239,5 +608,6 @@ def showHistory():
 
 
 Simulation_Mode()
+
 
 
